c3226ab00b51cf18cb8941d2d5d56f1dbff32ae6
jeanffc <jeanffc@gmail.com>
2020-05-19 21:55:38 -0300

    feat: created SignUp page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <h1>Faça seu Cadastro</h1>
+        <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
+        <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button type="submit">Cadastrar</Button>
+      </form>
+      <a href="new">
+        <FiArrowLeft />
+        Voltar
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
