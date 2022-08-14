17f4a9a9efc26da437691affdfe76cd3fcb3342e
Jean Fellipe Ferraz Campos <jeancampos@Jeans-MacBook-Pro.local>
2022-08-13 21:32:24 -0400

    feat: refact code

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -18,10 +18,8 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   const inputRef = useRef<HTMLInputElement>(null);
-
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
-
   const { fieldName, defaultValue, error, registerField } = useField(name);
 
   const handleInputFocus = useCallback(() => {
